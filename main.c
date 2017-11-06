25d304fbec29dfd94357019bd1ea62f2b41b0b3e
Codigos-de-Guerra <32881985+Codigos-de-Guerra@users.noreply.github.com>
2017-11-06 18:35:56 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,25 @@
+int main() {
+	FILE *imagem;//Ponteiro para o arquivo que contem a imagem de entrada
+	FILE *nova_imagem;//Ponteiro para o arquivo onde sera salva imagem resultante
+	char nome_imagem[50], nome_nova_imagem[50];
+
+	printf("Digite o nome do arquivo de entrada: ");
+	scanf("%s", nome_imagem);//Le o nome do arquivo de entrada
+	printf("Digite o nome de onde a nova imagem sera salva: ");
+	scanf("%s", nome_nova_imagem);
+
+	imagem = fopen(nome_imagem, "r");//Abre o arquivo de leitura da imagem
+	if (imagem == NULL) {
+		printf("Erro na abertura do arquivo %s\n", nome_imagem);
+		//stderr (fprintf(stderr,"msg de erro"))
+		return 0;
+	}
+
+	Imagem *minha_imagem_que_vai_ser_lida = malloc(sizeof(Imagem)); //;-;
+
+	ler_arq(imagem, minha_imagem_que_vai_ser_lida);
+
+	nova_imagem = fopen(nome_nova_imagem, "w");//Criando arquivo que recebera nova imagem
+	make_PPM_cinza(nova_imagem, minha_imagem_que_vai_ser_lida);
+
+}
