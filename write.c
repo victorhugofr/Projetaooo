28ef16008a38e4a4ed46489999dea6a8bbb75e7a
Codigos-de-Guerra <32881985+Codigos-de-Guerra@users.noreply.github.com>
2017-11-27 21:32:35 -0300

    Update and rename write_img.c to write.c

--- write.c
+++ write.c
@@ -1,5 +1,6 @@
-#include "write_img.h"
+#include "write.h"
 
+//Funcao de escrita da imagem passada por parametro, em um arquivo cujo nome tambem e passado por parametro
 void write_img(Imagem *m, char *outfilename) {
 	FILE* imagem;
 	unsigned int i,j;
@@ -7,9 +8,9 @@ void write_img(Imagem *m, char *outfilename) {
 	imagem = fopen(outfilename, "w");
 
 	//Inserindo o formato da imagem criada
-	fprintf(imagem, "%s\n#BY DANIEL O. GUERRA E PEDRO P. PARAENSE\n", m->header); //A imagem criada sera do mesmo tipo da imagem inserida
+	fprintf(imagem, "%s\n#BY DANIEL O. GUERRA E PEDRO AUGUSTO P. PESSOA\n", m->header); //A imagem criada sera do mesmo tipo da imagem inserida
 	//fprintf(imagem, "P3\n") Caso queira sempre em PPM
-
+	//Porem, em nosso caso, a imagem continua sempre sendo PPM
 
 	//Inserindo a largura e altura da matriz imagem, respectivamente
 	fprintf(imagem, "%u %u\n", m->largura, m->altura);
@@ -25,5 +26,6 @@ void write_img(Imagem *m, char *outfilename) {
 			fprintf(imagem, "%hu\n", m->M[i][j].b);
 		}
 	}
+
 	fclose(imagem);
 }
